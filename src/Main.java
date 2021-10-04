72381a3046684c30c260745453e9f514a51c1f1b
amschwartz <amschwartz87@gmail.com>
2021-10-04 15:29:51 +0300

    total

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        int price = 150;
-        int count = 5;
-        int total = price * count;
+        int currentBalance = 2_000_000_000;
+        int transferAmount = 500_000_000;
+        int total = currentBalance + transferAmount;
         System.out.println(total);
     }
 }
